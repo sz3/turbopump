a1e25b56617c7519f5976ef159cb5913a15a2342
sz <sz@galacticicecube.com>
2013-12-02 20:54:31 +0000

    Adding header only home for ezOptionParser. For ez... option parsing.

--- src/exe/turbopump/main.cpp
+++ src/exe/turbopump/main.cpp
@@ -1,10 +1,48 @@
 #include "main/TurboPumpApp.h"
 #include "programmable/TurboApi.h"
 
-int main()
+#include "optionparser/ezOptionParser.hpp"
+#include <string>
+using namespace ez;
+using std::string;
+
+int main(int argc, const char** argv)
 {
+	ezOptionParser opt;
+	opt.overview = "A high performance distributed key value store. 'high performance' is a relative term.";
+	opt.syntax = "./turbopump --dothething";
+	opt.footer = "turbopump 0.01";
+
+	opt.add("", false, 0, 0, "Display usage instructions.", "-h", "--help");
+	opt.add("/tmp/turbopump", false, 1, 0, "domain socket path", "-u", "--usepath");
+	opt.add("9001", false, 1, 0, "udp port", "-p", "--port");
+
+	opt.parse(argc, argv);
+
+	if (opt.isSet("-h"))
+	{
+		std::string usage;
+		opt.getUsage(usage);
+		std::cout << usage << std::endl;
+		return 1;
+	}
+	OptionGroup* og;
+
+	string turbopath;
+	if ((og = opt.get("--usepath")) != NULL)
+		og->getString(turbopath);
+
+	short port;
+	if ((og = opt.get("--port")) != NULL)
+	{
+		unsigned long temp;
+		og->getULong(temp);
+		port = temp;
+	}
+
+	std::cout << turbopath << ":" << port << std::endl;
 	TurboApi api;
-	TurboPumpApp app(api, "/tmp/turbopump", 9001);
+	TurboPumpApp app(api, turbopath, port);
 	app.run();
 	return 0;
 }
