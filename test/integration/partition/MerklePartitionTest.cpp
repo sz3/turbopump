b60e9422018263235acff810b637d4369e700490
sz <sz@galacticicecube.com>
2014-01-16 19:49:35 -0600

    Adding a simpler version of the merkle partitioning integration test. And this one passes! .. because it doesn't rely on the (currently nonexistent) drop functionality.

--- test/integration/partition/MerklePartitionTest.cpp
+++ test/integration/partition/MerklePartitionTest.cpp
@@ -38,6 +38,75 @@ TEST_CASE( "MerklePartitionTest/testSync", "[integration-udp]" )
 	response = cluster[1].query("ring");
 	assertEquals(expected, response);
 
+	// write each file to its primary location. Rely on healing to synchronize everything else.
+	// setting n=3 here, so we get 3 "proper" copies of the file instead of 2. (also, if we decide to change the defaults at any point...)
+	for (unsigned i = 1; i <= 6; ++i)
+	{
+		string num = StringUtil::str(i);
+		response = CommandLine::run("echo 'write|name=" + num + " n=3|hello" + num + "' | nc -U " + cluster[i].dataChannel());
+		assertEquals( "", response );
+	}
+
+	CommandLine::run("sleep 30");
+
+	// wait for files to propagate
+	// again, 2,6,1,5,4,3
+	response = cluster[1].query("local_list");
+	assertEquals( "(1)=>7\n"
+				  "(2)=>7\n"
+				  "(6)=>7", response );
+
+	response = cluster[2].query("local_list");
+	assertEquals( "(2)=>7\n"
+				  "(3)=>7\n"
+				  "(4)=>7", response );
+
+	response = cluster[3].query("local_list");
+	assertEquals( "(3)=>7\n"
+				  "(4)=>7\n"
+				  "(5)=>7", response );
+
+
+	response = cluster[4].query("local_list");
+	assertEquals( "(1)=>7\n"
+				  "(4)=>7\n"
+				  "(5)=>7", response );
+
+
+	response = cluster[5].query("local_list");
+	assertEquals( "(1)=>7\n"
+				  "(5)=>7\n"
+				  "(6)=>7", response );
+
+
+	response = cluster[6].query("local_list");
+	assertEquals( "(2)=>7\n"
+				  "(3)=>7\n"
+				  "(6)=>7", response );
+
+}
+
+TEST_CASE( "MerklePartitionTest/testRedistribute", "[integration-udp]" )
+{
+	TurboCluster cluster(6, "--udp --no-write-chaining");
+	cluster.start();
+	assertMsg( cluster.waitForRunning(), cluster.lastError() );
+
+	string response = cluster[1].query("membership");
+	assertEquals( "1 127.0.0.1:9001\n"
+				  "2 127.0.0.1:9002\n"
+				  "3 127.0.0.1:9003\n"
+				  "4 127.0.0.1:9004\n"
+				  "5 127.0.0.1:9005\n"
+				  "6 127.0.0.1:9006", response );
+
+	// the hash ring order is 2, 6, 1, 5, 4, 3.
+	// e.g. if the primary location is 2, the secondary is 6, the tertiary is 1...
+	string expected = hashRingStr("2") + hashRingStr("6") + hashRingStr("1") + hashRingStr("5") + hashRingStr("4") + hashRingStr("3");
+	response = cluster[1].query("ring");
+	assertEquals(expected, response);
+
+	// write ONLY to worker 1.
 	// setting n=3 here, so we get 3 "proper" copies of the file instead of 2. (also, if we decide to change the defaults at any point...)
 	for (unsigned i = 1; i <= 6; ++i)
 	{
