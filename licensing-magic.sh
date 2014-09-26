bdb39330ac211eee5fc781c1f80f627d9fce2ba8
sz <sz@galacticicecube.com>
2014-09-25 20:33:42 -0500

    Improved, less stupid, licensing-magic script.

--- licensing-magic.sh
+++ licensing-magic.sh
@@ -4,6 +4,11 @@ LICTEXT="/* This code is subject to the terms of the Mozilla Public License, v.2
 
 for f in $@
 do
+	if [[ $(head -1 $f) == *Mozilla\ Public\ License* ]]
+	then
+		continue
+	fi
+
 	echo "$LICTEXT"|cat - $f > /tmp/out && mv /tmp/out $f
 done
 
